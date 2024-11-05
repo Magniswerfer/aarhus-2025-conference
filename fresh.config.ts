e788dd71d853b146f8778a2186cd86df12784539
magniswerfer <magnushoholt@pm.me>
2024-11-05 17:17:12 +0100

    try again again again

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
+const isProduction = Deno.env.get("ENVIRONMENT") === "production";
+const basePath = isProduction ? "/aarhus2025-static" : "";
+
 export default defineConfig({
   plugins: [tailwind()],
   build: {
     target: ["chrome99", "firefox99", "safari12"],
     static: {
-      enabled: true,
-      directory: "_fresh"  // The directory where static files will be generated
+      enabled: true
     }
-  }
+  },
+  basePath
 });
 
