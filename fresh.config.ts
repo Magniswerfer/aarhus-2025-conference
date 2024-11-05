948ab2aa7d9017c07cc653983674005e3bfb6b76
magniswerfer <magnushoholt@pm.me>
2024-11-05 17:01:36 +0100

    new config

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,4 +3,12 @@ import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
   plugins: [tailwind()],
+  build: {
+    target: ["chrome99", "firefox99", "safari12"],
+    static: {
+      enabled: true,
+      directory: "_fresh"  // The directory where static files will be generated
+    }
+  }
 });
+
