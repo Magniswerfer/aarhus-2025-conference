873ac8f05bf6a8dab23ef152381306054104d4c8
magniswerfer <magnushoholt@pm.me>
2024-11-12 10:07:18 +0100

    the breaker works now

--- islands/XBreaker.tsx
+++ islands/XBreaker.tsx
@@ -1,26 +1,25 @@
 export default function XBreaker() {
   return (
-    <section class="bg-black h-64 md:h-80 lg:h-96 relative overflow-hidden">
+    <section class="bg-black h-64 md:h-64 xl:h-64 relative overflow-hidden">
       <div class="container mx-auto h-full flex items-center justify-center">
         {/* Left text group */}
-        <div class="absolute lg:relative left-4 md:left-8 lg:left-auto flex flex-col text-right z-10">
-          <div class="flex flex-col lg:flex-row items-end gap-1 md:gap-2 lg:gap-4 lg:mr-24">
-            <span class="text-white text-2xl md:text-4xl lg:text-6xl font-bold">
+        <div class="absolute xl:relative left-8 sm:left-20 md:left-16 lg:left-60 xl:left-auto flex flex-col text-right z-10">
+          <div class="flex flex-col xl:flex-row items-end gap-1 md:gap-2 xl:gap-4 xl:mr-24">
+            <span class="text-white text-2xl md:text-4xl xl:text-6xl font-bold">
               WHAT
             </span>
-            <span class="text-white text-2xl md:text-4xl lg:text-6xl font-bold">
+            <span class="text-white text-2xl md:text-4xl xl:text-6xl font-bold">
               DOES
             </span>
-            <span class="text-white text-2xl md:text-4xl lg:text-6xl font-bold">
+            <span class="text-white text-2xl md:text-4xl xl:text-6xl font-bold">
               YOUR
             </span>
           </div>
         </div>
-
         {/* Giant X */}
-        <div className="relative flex items-center justify-center h-full">
+        <div class="absolute xl:relative left-1/2 -translate-x-1/2 xl:left-auto xl:transform-none flex items-center justify-center h-full">
           <span
-            className="text-white leading-none absolute"
+            class="text-white leading-none absolute"
             style={{
               fontSize: "clamp(30rem, 60vw, 40rem)",
               transform: "translateY(-2%)",
@@ -29,14 +28,13 @@ export default function XBreaker() {
             X
           </span>
         </div>
-
         {/* Right text group */}
-        <div class="absolute lg:relative right-4 md:right-8 lg:right-auto flex flex-col text-left z-10">
-          <div class="flex flex-col lg:flex-row items-start gap-1 md:gap-2 lg:gap-4 lg:ml-24">
-            <span class="text-white text-2xl md:text-4xl lg:text-6xl font-bold">
+        <div class="absolute xl:relative right-8 sm:right-20 md:right-16 lg:right-60 xl:right-auto flex flex-col text-left z-10">
+          <div class="flex flex-col xl:flex-row items-start gap-1 md:gap-2 xl:gap-4 xl:ml-24">
+            <span class="text-white text-2xl md:text-4xl xl:text-6xl font-bold">
               STAND
             </span>
-            <span class="text-white text-2xl md:text-4xl lg:text-6xl font-bold">
+            <span class="text-white text-2xl md:text-4xl xl:text-6xl font-bold">
               FOR?
             </span>
           </div>
